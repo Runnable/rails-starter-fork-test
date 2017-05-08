2c58592d38679e35a4ba1c10c2f4af9d4d3fdc66
thejsj <jorge.silva@thejsj.com>
2017-05-08 13:35:09 -0700

    Add more retries

--- run_test.bash
+++ run_test.bash
@@ -1,9 +1,12 @@
 max_retries=${MAX_RETRIES:-200}
 retries=$max_retries
 
+echo "Start tests..."
+
 while [ $retries -gt 0 ] ; do
   echo "Attempting curl: Retries ${retries}"
   echo "Apphost: $APP_HOST"
+  curl -sL -w "%{http_code}\\n" "http://$APP_HOST:3000"
   exit_code=$(curl -sL -w "%{http_code}\\n" "http://$APP_HOST:3000" -o /dev/null)
   echo "Exit code: $exit_code"
   if [[ "$exit_code" == "200" ]]; then
@@ -11,7 +14,8 @@ while [ $retries -gt 0 ] ; do
     exit 0
   fi
   ((retries--))
-  sleep 1
+  echo "sleep 5"
+  sleep 5
 done
 
 echo "Out of retries"
